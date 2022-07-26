613626454ac25149c3f4ecf56ea4ff400914c0b9
Crowdbotics <team@crowdbotics.com>
2022-07-26 17:49:23 +0000

    Initial commit.

---
+++ backend/kafka_br_36428/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kafka_br_36428 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kafka_br_36428.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kafka_br_36428",
+  url: "https://kafka_br_36428.botics.co",
+  api: "https://kafka_br_36428.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
